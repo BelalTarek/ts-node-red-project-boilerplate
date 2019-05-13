efd25fc4c7257d3165add9494f976ac0ae999e2a
unknown <bishoymelekwadie@gmail.com>
2019-05-13 17:19:59 +0200

    fix flow path

---
+++ config/dev_settings.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+module.exports = {
+    uiPort: process.env.PORT || 3000,
+    flowFile: 'flow.json',
+    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "NODE_RED_SEC",
+    editorTheme: {
+        projects: {
+            enabled: true
+        }
+    }
+}
