dae4dbac1e0241beac2466b5b4537a8dfec82fce
unknown <bishoymelekwadie@gmail.com>
2019-05-14 18:32:04 +0200

    disable projects feature

--- config/dev_settings.js
+++ config/dev_settings.js
@@ -3,9 +3,4 @@ module.exports = {
     uiPort: process.env.PORT || 3000,
     flowFile: 'flow.json',
     credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "NODE_RED_SEC",
-    editorTheme: {
-        projects: {
-            enabled: true
-        }
-    }
 }
